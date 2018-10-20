422a04978e94f5c45865bce35fb90b099988c27d
Jakub Raban <jakub.raban@gmail.com>
2018-10-20 23:31:43 +0200

    Poprawki dodawania do kolekcji - zmiana pętli na Collections.addAll()

--- main/CardCollection.java
+++ main/CardCollection.java
@@ -3,9 +3,8 @@ import java.util.*;
 public class CardCollection {
 
     private LinkedList<Card> cards = new LinkedList<>();
-    Random r = new Random();
 
-    public CardCollection(boolean fillTheDeck) {
+    CardCollection(boolean fillTheDeck) {
         if(fillTheDeck) {
             for(int i = 0; i < 13; i++) {
                 for(int j = 0; j < 4; j++) {
@@ -16,21 +15,17 @@ public class CardCollection {
         Collections.shuffle(this.cards);
     }
 
-    public CardCollection(Card ... cards) {
-        for(int i = 0; i < cards.length; i++) {
-            this.cards.add(cards[i]);
-        }
+    CardCollection(Card ... cards) {
+        Collections.addAll(this.cards, cards);
         Collections.shuffle(this.cards);
     }
 
-    public void add(Card ... cards) {
-        for(int i = 0; i < cards.length; i++) {
-            this.cards.add(cards[i]);
-        }
+    void add(Card ... cards) {
+        Collections.addAll(this.cards, cards);
         Collections.shuffle(this.cards);
     }
 
-    public void add(CardCollection cards) {
+    void add(CardCollection cards) {
         int size = cards.size();
         for(int i = 0; i < size; i++) {
             this.cards.add(cards.play());
@@ -38,20 +33,20 @@ public class CardCollection {
         Collections.shuffle(this.cards);
     }
 
-    public void moveFrom(CardCollection collection) {
+    void moveFrom(CardCollection collection) {
         this.cards.addAll(collection.cards);
         Collections.shuffle(this.cards);
     }
 
-    public int size() {
+    int size() {
         return this.cards.size();
     }
 
-    public Card play() {
+    Card play() {
         return this.cards.poll();
     }
 
-    public void clear() {
+    void clear() {
         this.cards.clear();
     }
 
