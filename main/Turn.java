528e2923fe1b060e0f9b95a4fba5759fcbd8d71a
Jakub Raban <jakub.raban@gmail.com>
2018-10-20 23:07:35 +0200

    Poprawka komunikatu

--- main/Turn.java
+++ main/Turn.java
@@ -18,7 +18,7 @@ public class Turn {
             int comparision = c1.compareTo(c2);
             char comparisionSign = comparision > 0 ? '>' : (comparision == 0 ? '=' : '<');
             for(int i = 0; i < tieLevel; i++) toReturn.append("-> ");
-            if(tieLevel > 0) toReturn.append("Gracze dokładają kartę" + "\n");
+            if(tieLevel > 0) toReturn.append("Gracze dokładają zakrytą kartę" + "\n");
             toReturn.append(c1 + " " + comparisionSign + " " + c2);
             if(player1Cards.size() > 0) toReturn.append("\n");
             tieLevel++;
