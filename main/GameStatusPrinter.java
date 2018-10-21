e65d5d8e9bc8d2356a06fe8847f3dce2bdd7d116
Jakub Raban <jakub.raban@gmail.com>
2018-10-21 17:45:57 +0200

    Godzina w formacie 24h

--- main/GameStatusPrinter.java
+++ main/GameStatusPrinter.java
@@ -5,7 +5,6 @@ import java.util.Calendar;
 
 public class GameStatusPrinter {
 
-    private String filename;
     private PrintWriter printer;
 
     GameStatusPrinter() throws IOException {
@@ -13,11 +12,11 @@ public class GameStatusPrinter {
         int year = c.get(Calendar.YEAR);
         int month = c.get(Calendar.MONTH);
         int day = c.get(Calendar.DAY_OF_MONTH);
-        int hour = c.get(Calendar.HOUR);
+        int hour = c.get(Calendar.HOUR_OF_DAY);
         int minute = c.get(Calendar.MINUTE);
         int second = c.get(Calendar.SECOND);
-        filename = year + "-" + month + "-" + day + "_" + hour + "-" + minute + "-" + second + ".txt";
-        printer = new PrintWriter(filename, "UTF-8"); // TODO godzina w formacie 24h
+        String filename = year + "-" + month + "-" + day + "_" + hour + "-" + minute + "-" + second + ".txt";
+        printer = new PrintWriter(filename, "UTF-8");
     }
 
     void printToFileAndConsole(String text) {
